e1393c52b59d0acb14a804b97ce4a58e0fc85057
Luis <lmendez@neox.cl>
2024-01-26 20:19:18 -0300

    nombre flotante y cambio de imagen con efecto fadein

--- scripts.js
+++ scripts.js
@@ -3,24 +3,35 @@ document.body.classList.add("dark");
 
 const IMG_DARK_ASSET = 'assets/outline_dark_mode_black_24dp.png'; 
 const IMG_LIGHT_ASSET = 'assets/outline_light_mode_white_24dp.png'; 
+const IMG_DARK_PROFILE = 'assets/avatar_profile_reverencia.png'; 
+const IMG_LIGHT_PROFILE = 'assets/avatar_profile.png'; 
 
 function toggleDarkMode() {
   darkMode = !darkMode;
   const imgDarkButton = getById('dark_button_image');
+  const imgProfile = getById('profile_image');
+  imgProfile.classList.remove('visible');
+  
+  setTimeout(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+      imgDarkButton.setAttribute('src',IMG_LIGHT_ASSET);
+      imgProfile.setAttribute('src',IMG_LIGHT_PROFILE);
+      
+    } else {
+      document.body.classList.remove("dark");
+      imgDarkButton.setAttribute('src',IMG_DARK_ASSET);
+      imgProfile.setAttribute('src',IMG_DARK_PROFILE);
+    }
+    imgProfile.classList.add('visible');
+  }, 200);
 
-  if (darkMode) {
-    document.body.classList.add("dark");
-    imgDarkButton.setAttribute('src',IMG_LIGHT_ASSET);
-} else {
-    document.body.classList.remove("dark");
-    imgDarkButton.setAttribute('src',IMG_DARK_ASSET);
-  }
 }
+
+
 
 function selectCategory(el){
 
-  // console.log(el)
-  // const categorySelected = el.getAttribute('category');
   el.classList.add('active')
   const categories = document.getElementsByClassName('categories_item');
 
@@ -29,7 +40,7 @@ function selectCategory(el){
     const category = categories.item(index);
     
     if (el != category) {
-      category.classList.remove('active')
+      category.classList.remove('active');
     }
   }
   
